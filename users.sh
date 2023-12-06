925a0aa3cf8da0bf9f3492f9f8467569d9bd555b
Ubuntu <ubuntu@ip-172-31-33-154.us-east-2.compute.internal>
2023-12-06 16:46:50 +0000

    edit

--- users.sh
+++ users.sh
@@ -14,4 +14,14 @@ python manage.py create_users -user mmeza -pass m.meza
 
 python manage.py create_users -user lestienne -pass l.estienne
 
-python manage.py create_users -user gbujia -pass g.bujia
+python manage.py create_users -user user1 -pass user.1
+
+python manage.py create_users -user user2 -pass user.2
+
+python manage.py create_users -user user3 -pass user.3
+
+python manage.py create_users -user user4 -pass user.4
+
+python manage.py create_users -user user5 -pass user.5
+
+python manage.py create_users -user user6 -pass user.6
