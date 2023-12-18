9a11eed9f97e4446682ced6d699a9a65e4434455
Ubuntu <ubuntu@ip-172-31-33-154.us-east-2.compute.internal>
2023-12-18 01:00:01 -0300

    update_backup

--- users.sh
+++ users.sh
@@ -1,27 +1,24 @@
 python manage.py create_users -user lferrer -pass l.ferrer
-
 python manage.py create_users -user mgauder -pass m.gauder
-
 python manage.py create_users -user cbonomi -pass c.bonomi
-
 python manage.py create_users -user gbarchi -pass g.barchi
-
 python manage.py create_users -user marcos -pass marcos
-
 python manage.py create_users -user mari -pass mari
-
 python manage.py create_users -user mmeza -pass m.meza
-
 python manage.py create_users -user lestienne -pass l.estienne
-
 python manage.py create_users -user user1 -pass user.1
-
 python manage.py create_users -user user2 -pass user.2
-
 python manage.py create_users -user user3 -pass user.3
-
 python manage.py create_users -user user4 -pass user.4
-
 python manage.py create_users -user user5 -pass user.5
-
 python manage.py create_users -user user6 -pass user.6
+python manage.py create_users -user user7 -pass user.7
+python manage.py create_users -user user8 -pass user.8
+python manage.py create_users -user user9 -pass user.9
+python manage.py create_users -user user10 -pass user.10
+python manage.py create_users -user user11 -pass user.11
+python manage.py create_users -user user12 -pass user.12
+python manage.py create_users -user user13 -pass user.13
+python manage.py create_users -user user14 -pass user.14
+python manage.py create_users -user user15 -pass user.15
+
